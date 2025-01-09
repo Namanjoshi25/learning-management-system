7fb0306c08ca9feadf045bfcff8ecb28aaf61e03
Naman joshi <84171358+Namanjoshi25@users.noreply.github.com>
2025-01-09 21:41:31 +0530

    final project

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,12 @@ const nextConfig = {
     images:{
         domains:["utfs.io"]
     },
-    typescript: {
-        ignoreBuildErrors: true,
-      }
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
+    
 };
 
 export default nextConfig;
